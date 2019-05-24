dc3f1ba26f4f89aa39bc715df6d51d6df37510f2
Christian Lim <christiananthonylim1@gmail.com>
2019-05-24 21:45:34 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -15,7 +15,7 @@ def third_element(array)
   array[2]
 end
 
-def last_element
+def last_element(array)
   array[-1]
 end
 
@@ -26,3 +26,9 @@ end
 def last_element_with_array_methods(array)
   array.last
 end
+
+def length_of_array
+  array.length
+end
+
+
